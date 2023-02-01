c42a1e0c98d78e838d177e5972a21d278fd6c68a
Priyanshu Sharma <sharmapriyanshu7164196@gmail.com>
2023-02-01 08:44:12 +0530

    Inheritance Challenge

---
+++ Inheritance Challenge/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main (String[] args) {
+
+        Employee priyanshu = new Employee ("Priyanshu", "23/01/2001", "01/01/2020");
+        System.out.println (priyanshu);
+        System.out.println ("Age = " + priyanshu.getAge ());
+        System.out.println ("Pay = " + priyanshu.collectPay ());
+
+        SalariedEmployee yash = new SalariedEmployee ("Yash", "02/09/2001", "03/03/2020", 35000);
+        System.out.println (yash);
+        System.out.println ("Joe's Paycheck = $" + yash.collectPay ());
+
+        yash.retire ();
+        System.out.println ("Joe's Pension check = $" + yash.collectPay ());
+
+        HourlyEmployee swati = new HourlyEmployee ("Swati", "24/02/2000", "03/03/2021", 15);
+        System.out.println (swati);
+        System.out.println ("Mary's Paycheck = $" + swati.collectPay ());
+        System.out.println ("Mary's Holiday Pay = $" + swati.getDoublePay ());
+    }
+}
