8a975081979fa5f83382eb967a1c44215415bc2c
Priyanshu Sharma <sharmapriyanshu7164196@gmail.com>
2023-02-03 16:01:01 +0530

    edit

--- Inheritance Challenge/src/Main.java
+++ Inheritance Challenge/src/Main.java
@@ -9,14 +9,14 @@ public class Main {
 
         SalariedEmployee yash = new SalariedEmployee ("Yash", "02/09/2001", "03/03/2020", 35000);
         System.out.println (yash);
-        System.out.println ("Joe's Paycheck = $" + yash.collectPay ());
+        System.out.println ("Yash's Paycheck = $" + yash.collectPay ());
 
         yash.retire ();
-        System.out.println ("Joe's Pension check = $" + yash.collectPay ());
+        System.out.println ("Yash's Pension check = $" + yash.collectPay ());
 
         HourlyEmployee swati = new HourlyEmployee ("Swati", "24/02/2000", "03/03/2021", 15);
         System.out.println (swati);
-        System.out.println ("Mary's Paycheck = $" + swati.collectPay ());
-        System.out.println ("Mary's Holiday Pay = $" + swati.getDoublePay ());
+        System.out.println ("Swati's Paycheck = $" + swati.collectPay ());
+        System.out.println ("Swati's Holiday Pay = $" + swati.getDoublePay ());
     }
 }
