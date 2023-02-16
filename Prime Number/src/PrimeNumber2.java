775cd4a9d04e8595cc28c91b094b24936a726f68
Priyanshu Sharma <sharmapriyanshu7164196@gmail.com>
2023-02-16 08:55:48 +0530

    Prime Number

---
+++ Prime Number/src/PrimeNumber2.java
@@ -0,0 +1,30 @@
+/*Prime number program using method*/
+public class PrimeNumber2 {
+
+    public static void main (String[] args) {
+
+        checkPrime (1);
+        checkPrime (3);
+        checkPrime (17);
+        checkPrime (20);
+    }
+
+    static void checkPrime (int n) {
+        int i, m = 0, flag = 0;
+        m = n / 2;
+        if ( n == 0 || n == 1 ) {
+            System.out.println (n + " is not prime number");
+        } else {
+            for (i = 2; i <= m; i++) {
+                if ( n % i == 0 ) {
+                    System.out.println (n + " is not prime number");
+                    flag = 1;
+                    break;
+                }
+            }
+            if ( flag == 0 ) {
+                System.out.println (n + " is prime number");
+            }
+        }
+    }
+}
