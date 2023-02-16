278a8001e7553528d2613522dec382a4b9e0b208
Priyanshu Sharma <sharmapriyanshu7164196@gmail.com>
2023-02-16 11:20:56 +0530

    Abstract Class Challenge

---
+++ Abstract Class Challenge/src/sharma/priyanshu/Furniture.java
@@ -0,0 +1,15 @@
+package sharma.priyanshu;
+
+public class Furniture extends ProductForSale {
+
+    public Furniture (String type, double price, String description) {
+        super (type, price, description);
+    }
+
+    @Override
+    public void showDetails () {
+        System.out.println ("This " + type + " was manufactured in North Carolina");
+        System.out.printf ("The price of the piece is ₹%6.2f %n", price);
+        System.out.println (description);
+    }
+}
---
+++ Abstract Class Challenge/src/sharma/priyanshu/ProductForSale.java
@@ -0,0 +1,25 @@
+package sharma.priyanshu;
+
+public abstract class ProductForSale {
+
+    protected String type;
+    protected double price;
+    protected String description;
+
+    public ProductForSale (String type, double price, String description) {
+        this.type = type;
+        this.price = price;
+        this.description = description;
+    }
+
+    public double getSalesPrice (int qty) {
+        return qty * price;
+    }
+
+    public void printPricedItem (int qty) {
+
+        System.out.printf ("%2d qty at ₹%8.2f each,  %-15s %-35s %n", qty, price, type, description);
+    }
+
+    public abstract void showDetails ();
+}
