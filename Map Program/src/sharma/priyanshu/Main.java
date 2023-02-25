147d2f998c65493597a607aa028e38543db0194b
Priyanshu Sharma <sharmapriyanshu7164196@gmail.com>
2023-02-25 11:34:33 +0530

    edit map project

--- Map Program/src/sharma/priyanshu/Main.java
+++ Map Program/src/sharma/priyanshu/Main.java
@@ -28,6 +28,20 @@ public class Main {
 
         System.out.println ("=".repeat (100));
 
+//        languages.remove("Lisp");
+        if ( languages.remove ("Algol", "an algorithmic language") ) {
+            System.out.println ("Algol removed");
+        } else {
+            System.out.println ("Algol not removed, key/value pair not found");
+        }
+
+        if ( languages.replace ("Lisp", "Therein lies madness", "a functional programming language with imperative features") ) {
+            System.out.println ("Lisp replaced");
+        } else {
+            System.out.println ("Lisp was not replaced");
+        }
+//        System.out.println(languages.replace("Scala", "this will not be added"));
+
         for (String key : languages.keySet ()) {
             System.out.println (key + " : " + languages.get (key));
         }
