39377a8293adaf0aaa81471eef5a7d94d55e703b
Priyanshu Sharma <sharmapriyanshu7164196@gmail.com>
2023-02-25 17:24:08 +0530

    Add exit to adventure

--- Adventure/src/sharma/priyanshu/Location.java
+++ Adventure/src/sharma/priyanshu/Location.java
@@ -13,6 +13,7 @@ public class Location {
         this.locationID = locationID;
         this.description = description;
         this.exits = new HashMap<> ();
+        this.exits.put ("Q", 0);
     }
 
     public void addExit (String direction, int location) {
--- Adventure/src/sharma/priyanshu/Main.java
+++ Adventure/src/sharma/priyanshu/Main.java
@@ -9,7 +9,6 @@ public class Main {
     private static final Map<Integer, Location> locations = new HashMap<> ();
 
     public static void main (String[] args) {
-
         Scanner scanner = new Scanner (System.in);
 
         locations.put (0, new Location (0, "You are sitting in front of a computer learning Java"));
@@ -19,16 +18,47 @@ public class Main {
         locations.put (4, new Location (4, "You are in a valley beside a stream"));
         locations.put (5, new Location (5, "You are in the forest"));
 
+        locations.get (1).addExit ("W", 2);
+        locations.get (1).addExit ("E", 3);
+        locations.get (1).addExit ("S", 4);
+        locations.get (1).addExit ("N", 5);
+//        locations.get(1).addExit("Q", 0);
+
+        locations.get (2).addExit ("N", 5);
+//        locations.get(2).addExit("Q", 0);
+
+        locations.get (3).addExit ("W", 1);
+//        locations.get(3).addExit("Q", 0);
+
+        locations.get (4).addExit ("N", 1);
+        locations.get (4).addExit ("W", 2);
+//        locations.get(4).addExit("Q", 0);
+
+        locations.get (5).addExit ("S", 1);
+        locations.get (5).addExit ("W", 2);
+//        locations.get(5).addExit("Q", 0);
+
         int loc = 1;
-        while (true) {
-            System.out.println (locations.get (loc).getDescription ());
-            if ( loc == 0 ) {
+        while(true) {
+            System.out.println(locations.get(loc).getDescription());
+            if(loc == 0) {
                 break;
             }
 
-            loc = scanner.nextInt ();
-            if ( !locations.containsKey (loc) ) {
-                System.out.println ("You cannot go in that direction");
+            Map<String, Integer> exits = locations.get(loc).getExits();
+            System.out.print("Available exits are ");
+            for(String exit: exits.keySet()) {
+                System.out.print(exit + ", ");
+            }
+            System.out.println();
+
+            String direction = scanner.nextLine().toUpperCase();
+
+            if(exits.containsKey(direction)) {
+                loc = exits.get(direction);
+
+            } else {
+                System.out.println("You cannot go in that direction");
             }
         }
     }
