516f7ef3adfb6da18b179ceea97c5c9e472d8265
Priyanshu Sharma <sharmapriyanshu7164196@gmail.com>
2023-03-04 07:04:08 +0530

    Set Challenge

---
+++ Set Challenge/src/sharma/priyanshu/DwarfPlanet.java
@@ -0,0 +1,8 @@
+package sharma.priyanshu;
+
+public class DwarfPlanet extends HeavenlyBody {
+
+    public DwarfPlanet (String name, double orbitalPeriod) {
+        super (name, orbitalPeriod, BodyTypes.DWARF_PLANET);
+    }
+}
---
+++ Set Challenge/src/sharma/priyanshu/Moon.java
@@ -0,0 +1,8 @@
+package sharma.priyanshu;
+
+public class Moon extends HeavenlyBody {
+
+    public Moon (String name, double orbitalPeriod) {
+        super (name, orbitalPeriod, BodyTypes.MOON);
+    }
+}
