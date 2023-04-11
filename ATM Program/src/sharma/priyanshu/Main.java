395072cdbe212faafefafed757ca8280b5379239
Priyanshu Sharma <sharmapriyanshu7164196@gmail.com>
2023-04-11 09:25:38 +0530

    ATM Program

---
+++ ATM Program/src/sharma/priyanshu/Main.java
@@ -0,0 +1,47 @@
+package sharma.priyanshu;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main (String[] args) {
+        int balance = 100000, withdraw, deposit;
+        Scanner scanner = new Scanner (System.in);
+
+        while (true) {
+            System.out.println ("Automated Teller Machine");
+            System.out.println ("Choose 1 for Withdraw");
+            System.out.println ("Choose 2 for Deposit");
+            System.out.println ("Choose 3 for Check Balance");
+            System.out.println ("Choose 4 for EXIT");
+            System.out.print ("Choose the operation you want to perform:");
+            int choice = scanner.nextInt ();
+
+            switch (choice) {
+                case 1 -> {
+                    System.out.print ("Enter money to be withdrawn:");
+                    withdraw = scanner.nextInt ();
+                    if ( balance >= withdraw ) {
+                        balance = balance - withdraw;
+                        System.out.println ("Please collect your money");
+                    } else {
+                        System.out.println ("Insufficient Balance");
+                    }
+                    System.out.println ();
+                }
+                case 2 -> {
+                    System.out.print ("Enter money to be deposited:");
+                    deposit = scanner.nextInt ();
+                    balance = balance + deposit;
+                    System.out.println ("Your Money has been successfully depsited");
+                    System.out.println ();
+                }
+                case 3 -> {
+                    System.out.println ("Balance : " + balance);
+                    System.out.println ();
+                }
+                case 4 -> System.exit (0);
+            }
+        }
+    }
+}
