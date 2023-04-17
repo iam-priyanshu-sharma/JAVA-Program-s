c0650b5d51c287b4d30495e047bac9eaee26844d
Priyanshu Sharma <sharmapriyanshu7164196@gmail.com>
2023-04-17 10:33:50 +0530

    Program to print the smallest element in an array

--- Program to print the smallest element in an array/src/sharma/priyanshu/Main.java
+++ Program to print the smallest element in an array/src/sharma/priyanshu/Main.java
@@ -4,13 +4,13 @@ public class Main {
 
     public static void main (String[] args) {
         int[] arr = new int[]{25, 11, 7, 75, 56};
-        int max = arr[0];
+        int min = arr[0];
 
         for (int j : arr) {
-            if ( j > max )
-                max = j;
+            if ( j < min )
+                min = j;
         }
-
-        System.out.println ("Largest element present in given array: " + max);
+        
+        System.out.println ("Smallest element present in given array: " + min);
     }
 }
