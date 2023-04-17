3591db617e952783755ef152a61d6b8d69eaf6a7
Priyanshu Sharma <sharmapriyanshu7164196@gmail.com>
2023-04-17 10:23:57 +0530

    Program to print the elements of an array present on odd position

--- Program to print the elements of an array in reverse order/src/sharma/priyanshu/Main.java
+++ Program to print the elements of an array in reverse order/src/sharma/priyanshu/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main (String[] args) {
         int[] arr = new int[]{1, 2, 3, 4, 5};
         System.out.println ("Original array: ");
-        
+
         for (int j : arr) {
             System.out.print (j + " ");
         }
