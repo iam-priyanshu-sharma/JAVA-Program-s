69a51a040ec15154624474ece410db3f0eca879a
Priyanshu Sharma <sharmapriyanshu7164196@gmail.com>
2023-04-17 10:06:13 +0530

    Program to print the elements of an array

---
+++ Program to print the elements of an array/src/sharma/priyanshu/Main.java
@@ -0,0 +1,13 @@
+package sharma.priyanshu;
+
+public class Main {
+
+    public static void main (String[] args) {
+        int[] arr = new int[]{1, 2, 3, 4, 5};
+        System.out.println ("Elements of given array: ");
+
+        for (int j : arr) {
+            System.out.print (j + " ");
+        }
+    }
+}
