179b31a712a6f3ee1931f7e1bf8721a1010e4219
Priyanshu Sharma <sharmapriyanshu7164196@gmail.com>
2023-04-17 10:17:40 +0530

    Program to print the elements of an array in reverse order

--- Program to print the duplicate elements of an array/src/sharma/priyanshu/Main.java
+++ Program to print the duplicate elements of an array/src/sharma/priyanshu/Main.java
@@ -9,7 +9,7 @@ public class Main {
         for (int i = 0; i < arr.length; i++) {
 
             for (int j = i + 1; j < arr.length; j++) {
-                
+
                 if ( arr[i] == arr[j] )
                     System.out.println (arr[j]);
             }
---
+++ Program to print the elements of an array in reverse order/src/sharma/priyanshu/Main.java
@@ -0,0 +1,20 @@
+package sharma.priyanshu;
+
+public class Main {
+
+    public static void main (String[] args) {
+        int[] arr = new int[]{1, 2, 3, 4, 5};
+        System.out.println ("Original array: ");
+        
+        for (int j : arr) {
+            System.out.print (j + " ");
+        }
+
+        System.out.println ();
+        System.out.println ("Array in reverse order: ");
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print (arr[i] + " ");
+        }
+    }
+}
