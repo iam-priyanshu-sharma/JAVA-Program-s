c1bcc0b4cd60868b73c25db6127effca2de8fb7c
Priyanshu Sharma <sharmapriyanshu7164196@gmail.com>
2023-04-17 10:52:33 +0530

    Java Program to print the sum of all the items of the array

--- Java Program to print the number of elements present in an array/src/sharma/priyanshu/Main.java
+++ Java Program to print the number of elements present in an array/src/sharma/priyanshu/Main.java
@@ -1,2 +1,9 @@
-package sharma.priyanshu;public class Main {
+package sharma.priyanshu;
+
+public class Main {
+
+    public static void main (String[] args) {
+        int[] arr = new int[]{1, 2, 3, 4, 5};
+        System.out.println ("Number of elements present in given array: " + arr.length);
+    }
 }
---
+++ Java Program to print the sum of all the items of the array/src/sharma/priyanshu/Main.java
@@ -0,0 +1,15 @@
+package sharma.priyanshu;
+
+public class Main {
+
+    public static void main (String[] args) {
+        int[] arr = new int[]{1, 2, 3, 4, 5};
+        int sum = 0;
+
+        for (int j : arr) {
+            sum = sum + j;
+        }
+        
+        System.out.println ("Sum of all the elements of an array: " + sum);
+    }
+}
