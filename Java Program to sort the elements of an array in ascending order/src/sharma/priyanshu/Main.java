e0108d504ef283063660f12e0e532bebf4325d32
Priyanshu Sharma <sharmapriyanshu7164196@gmail.com>
2023-04-17 10:59:17 +0530

    Java Program to sort the elements of an array in ascending order

---
+++ Java Program to sort the elements of an array in ascending order/src/sharma/priyanshu/Main.java
@@ -0,0 +1,31 @@
+package sharma.priyanshu;
+
+public class Main {
+
+    public static void main (String[] args) {
+        int[] arr = new int[]{5, 2, 8, 7, 1};
+        int temp = 0;
+        System.out.println ("Elements of original array: ");
+
+        for (int k : arr) {
+            System.out.print (k + " ");
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if ( arr[i] > arr[j] ) {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        System.out.println ();
+        System.out.println ("Elements of array sorted in ascending order: ");
+
+        for (int j : arr) {
+            System.out.print (j + " ");
+        }
+    }
+}
