88aa581e54002ffa783376083680e334a2bc0af3
Priyanshu Sharma <sharmapriyanshu7164196@gmail.com>
2023-04-18 09:17:51 +0530

    Java Program to find Smallest Number in an Array

---
+++ Java Program to find Smallest Number in an Array/src/sharma/priyanshu/Collection.java
@@ -0,0 +1,21 @@
+package sharma.priyanshu;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Collection {
+
+    public static void main (String[] args) {
+        Integer[] a = {1, 2, 5, 6, 3, 2};
+        Integer[] b = {44, 66, 99, 77, 33, 22, 55};
+        System.out.println ("Smallest: " + getSmallest (a, 6));
+        System.out.println ("Smallest: " + getSmallest (b, 7));
+    }
+
+    public static int getSmallest (Integer[] a, int total) {
+        List<Integer> list = Arrays.asList (a);
+        Collections.sort (list);
+        return list.get (0);
+    }
+}
