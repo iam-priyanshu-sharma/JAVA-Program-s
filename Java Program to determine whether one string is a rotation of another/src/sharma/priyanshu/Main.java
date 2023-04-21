0f6246e9d1bb71634fee6843c8eb9ece99d4ec89
Priyanshu Sharma <sharmapriyanshu7164196@gmail.com>
2023-04-21 19:22:30 +0530

    Java Program to determine whether one string is a rotation of another

--- Java Program to determine whether one string is a rotation of another/src/sharma/priyanshu/Main.java
+++ Java Program to determine whether one string is a rotation of another/src/sharma/priyanshu/Main.java
@@ -9,7 +9,7 @@ public class Main {
             System.out.println ("Second string is not a rotation of first string");
         } else {
             str1 = str1.concat (str1);
-            
+
             if ( str1.contains (str2) ) {
                 System.out.println ("Second string is a rotation of first string");
             } else {
