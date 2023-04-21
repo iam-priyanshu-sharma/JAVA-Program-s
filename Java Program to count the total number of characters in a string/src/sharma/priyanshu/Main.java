d2d162e922c8933a904e794ae2b4887200abc93f
Priyanshu Sharma <sharmapriyanshu7164196@gmail.com>
2023-04-21 17:57:27 +0530

    Java Program to count the total number of characters in a string

---
+++ Java Program to count the total number of characters in a string/src/sharma/priyanshu/Main.java
@@ -0,0 +1,16 @@
+package sharma.priyanshu;
+
+public class Main {
+
+    public static void main (String[] args) {
+        String string = "The best of both worlds";
+        int count = 0;
+
+        for (int i = 0; i < string.length (); i++) {
+            if ( string.charAt (i) != ' ' )
+                count++;
+        }
+
+        System.out.println ("Total number of characters in a string: " + count);
+    }
+}
