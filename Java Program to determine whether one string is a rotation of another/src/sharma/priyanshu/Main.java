7c63b8deddc932ed6ee501200842ae074b271bf0
Priyanshu Sharma <sharmapriyanshu7164196@gmail.com>
2023-04-21 19:02:27 +0530

    Java Program to determine whether a given string is palindrome

---
+++ Java Program to determine whether one string is a rotation of another/src/sharma/priyanshu/Main.java
@@ -0,0 +1,20 @@
+package sharma.priyanshu;
+
+public class Main {
+
+    public static void main (String[] args) {
+        String str1 = "abcde", str2 = "deabc";
+
+        if ( str1.length () != str2.length () ) {
+            System.out.println ("Second string is not a rotation of first string");
+        } else {
+            str1 = str1.concat (str1);
+            
+            if ( str1.contains (str2) ) {
+                System.out.println ("Second string is a rotation of first string");
+            } else {
+                System.out.println ("Second string is not a rotation of first string");
+            }
+        }
+    }
+}
