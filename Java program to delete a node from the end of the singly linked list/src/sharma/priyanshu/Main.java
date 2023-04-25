844fcf849562f7be3e7dbe2df8d19d38d1abc1ce
Priyanshu Sharma <sharmapriyanshu7164196@gmail.com>
2023-04-25 10:45:20 +0530

    Java program to delete a node from the end of the singly linked list

---
+++ Java program to delete a node from the end of the singly linked list/src/sharma/priyanshu/Main.java
@@ -0,0 +1,80 @@
+package sharma.priyanshu;
+
+public class Main {
+
+    public Node head = null;
+    public Node tail = null;
+
+    public static void main (String[] args) {
+        Main sList = new Main ();
+        sList.addNode (1);
+        sList.addNode (2);
+        sList.addNode (3);
+        sList.addNode (4);
+
+        System.out.println ("Original List: ");
+        sList.display ();
+
+        while (sList.head != null) {
+            sList.deleteFromEnd ();
+            System.out.println ("Updated List: ");
+            sList.display ();
+        }
+    }
+
+    public void addNode (int data) {
+        Node newNode = new Node (data);
+
+        if ( head == null ) {
+            head = newNode;
+        } else {
+            tail.next = newNode;
+        }
+
+        tail = newNode;
+    }
+
+    public void deleteFromEnd () {
+        if ( head == null ) {
+            System.out.println ("List is empty");
+        } else {
+            if ( head == tail ) {
+                head = tail = null;
+            } else {
+                Node current = head;
+
+                while (current.next != tail) {
+                    current = current.next;
+                }
+                tail = current;
+                tail.next = null;
+            }
+        }
+    }
+
+    public void display () {
+        Node current = head;
+
+        if ( head == null ) {
+            System.out.println ("List is empty");
+            return;
+        }
+
+        while (current != null) {
+            System.out.print (current.data + " ");
+            current = current.next;
+        }
+
+        System.out.println ();
+    }
+
+    static class Node {
+        int data;
+        Node next;
+
+        public Node (int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+}
