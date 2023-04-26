d45e88f12dcfa3f67f1565acf7d6f0f67878a8e6
Priyanshu Sharma <sharmapriyanshu7164196@gmail.com>
2023-04-26 08:47:41 +0530

    Java program to delete a node from the beginning of the Circular Linked List

---
+++ Java program to delete a node from the beginning of the Circular Linked List/src/sharma/priyanshu/Main.java
@@ -0,0 +1,75 @@
+package sharma.priyanshu;
+
+public class Main {
+
+    public Node head = null;
+    public Node tail = null;
+
+    public static void main (String[] args) {
+        Main cl = new Main ();
+
+        cl.add (1);
+        cl.add (2);
+        cl.add (3);
+        cl.add (4);
+
+        System.out.println ("Original List: ");
+        cl.display ();
+
+        while (cl.head != null) {
+            cl.deleteStart ();
+            System.out.println ("Updated List: ");
+            cl.display ();
+        }
+    }
+
+    public void add (int data) {
+        Node newNode = new Node (data);
+
+        if ( head == null ) {
+            head = newNode;
+            tail = newNode;
+            newNode.next = head;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+            tail.next = head;
+        }
+    }
+
+    public void deleteStart () {
+        if ( head == null ) {
+        } else {
+            if ( head != tail ) {
+                head = head.next;
+                tail.next = head;
+            } else {
+                head = tail = null;
+            }
+        }
+    }
+
+    public void display () {
+        Node current = head;
+
+        if ( head == null ) {
+            System.out.println ("List is empty");
+        } else {
+            do {
+                System.out.print (" " + current.data);
+                current = current.next;
+            } while (current != head);
+
+            System.out.println ();
+        }
+    }
+
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node (int data) {
+            this.data = data;
+        }
+    }
+}
