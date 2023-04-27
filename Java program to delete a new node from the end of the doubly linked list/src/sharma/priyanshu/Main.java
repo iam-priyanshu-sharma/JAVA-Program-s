403047a64c02cafc7b2fb56681b5da49c5929141
Priyanshu Sharma <sharmapriyanshu7164196@gmail.com>
2023-04-27 09:31:25 +0530

    Java program to delete a new node from the end of the doubly linked list

---
+++ Java program to delete a new node from the end of the doubly linked list/src/sharma/priyanshu/Main.java
@@ -0,0 +1,80 @@
+package sharma.priyanshu;
+
+public class Main {
+
+    Node head, tail = null;
+
+    public static void main (String[] args) {
+
+        Main dList = new Main ();
+
+        dList.addNode (1);
+        dList.addNode (2);
+        dList.addNode (3);
+        dList.addNode (4);
+        dList.addNode (5);
+
+        System.out.println ("Original List: ");
+        dList.display ();
+
+        while (dList.head != null) {
+            dList.deleteFromEnd ();
+            System.out.println ("Updated List: ");
+            dList.display ();
+        }
+    }
+
+    public void addNode (int data) {
+        Node newNode = new Node (data);
+
+        if ( head == null ) {
+            head = tail = newNode;
+            head.previous = null;
+        } else {
+            tail.next = newNode;
+            newNode.previous = tail;
+            tail = newNode;
+        }
+
+        tail.next = null;
+    }
+
+    public void deleteFromEnd () {
+        if ( head == null ) {
+            return;
+        } else {
+            if ( head != tail ) {
+                tail = tail.previous;
+                tail.next = null;
+            } else {
+                head = tail = null;
+            }
+        }
+    }
+
+    public void display () {
+        Node current = head;
+
+        if ( head == null ) {
+            System.out.println ("List is empty");
+            return;
+        }
+
+        while (current != null) {
+            System.out.print (current.data + " ");
+            current = current.next;
+        }
+
+        System.out.println ();
+    }
+
+    static class Node {
+        int data;
+        Node previous;
+        Node next;
+
+        public Node (int data) {
+            this.data = data;
+        }
+    }
+}
